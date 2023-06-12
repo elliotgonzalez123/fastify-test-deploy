ad8591b15c7a4d229b1888a58ca54a7c89a4c4ef
Elliot Gonzalez <elliotgrey@gmail.com>
2023-06-12 13:36:07 -0400

    port

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,11 @@ const fastify: FastifyInstance = Fastify({ logger: true });
 
 fastify.register(UserRoutes, { prefix: "/api/users" });
 
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    await fastify.listen({ port: PORT, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
